35df5abf238d70a6d7f74bc699f49d5243d5c267
Jon Jee <jydrogen@gmail.com>
2019-05-19 21:13:04 +0900

    plugin: index.html 업로드하면 Plugin 작동하는 것 확인

--- src/components/PluginView.tsx
+++ src/components/PluginView.tsx
@@ -7,14 +7,19 @@ interface PluginViewProps {
 }
 
 class PluginView extends Component<PluginViewProps> {
-    script: RefObject<any>;
+
     constructor(props: PluginViewProps) {
         super(props);
-        this.script = React.createRef();
     }
+
     componentDidMount() {
-        const script = document.getElementById('playground-script').innerHTML;
-        window.eval(script);
+        let plugin = this.props.plugin;
+        plugin = plugin.replace(/\n/g, '');      // HTML Code 개행 제거 (정규표현식을 위해)
+        const extractscript = /<script>(.+)<\/script>/gim.exec(plugin);
+        if (extractscript) {
+            plugin=plugin.replace(extractscript[0],"");
+            new Function(extractscript[1])();
+        }
     }
 
     render() {
