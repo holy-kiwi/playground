6cf901ca8d5d0de927cf80db66f6eec623b169df
Jon Jee <jydrogen@gmail.com>
2019-09-03 13:56:03 +0900

    DndProvider를 HomeScreen 컴포넌트 안으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { Provider } from 'mobx-react';
 import { DndProvider } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+
 import Stores from './stores/export';
 import HomeScreen from './screens/HomeScreen';
 import StoreScreen from './screens/StoreScreen';
@@ -11,10 +11,8 @@ function App() {
   return (
     <div>
       <Provider {...Stores}>
-        <DndProvider backend={HTML5Backend}>
-          <Route exact path="/" component={HomeScreen} />
-          <Route path="/store" component={StoreScreen} />
-        </DndProvider>
+        <Route exact path="/" component={HomeScreen} />
+        <Route path="/store" component={StoreScreen} />
       </Provider>
     </div>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import './HomeScreen.css';
 import UploaderView from '../components/UploaderView';
 import Container from '../components/Container';
@@ -24,19 +26,21 @@ class HomeScreen extends Component<HomeScreenProps, HomeScreenState> {
     render() {
         const { HomeScreenStore } = this.props;
         return (
-            <div className="HomeScreenContainer">
-                <UploaderView />
-                <Container />
-                <div className="MenuContainer">
-                    <Link to='/store'><button>스토어</button></Link>
-                    <button onClick={() => { HomeScreenStore.toggleEditMode() }}>편집</button>
-                    <button onClick={() => {
-                        HomeScreenStore.setPlugins({});
-                        LocalStorageUtil.setPlugins(HomeScreenStore.plugins);
-                        localStorage.clear();
-                    }}>초기화</button>
+            <DndProvider backend={HTML5Backend}>
+                <div className="HomeScreenContainer">
+                    <UploaderView />
+                    <Container />
+                    <div className="MenuContainer">
+                        <Link to='/store'><button>스토어</button></Link>
+                        <button onClick={() => { HomeScreenStore.toggleEditMode() }}>편집</button>
+                        <button onClick={() => {
+                            HomeScreenStore.setPlugins({});
+                            LocalStorageUtil.setPlugins(HomeScreenStore.plugins);
+                            localStorage.clear();
+                        }}>초기화</button>
+                    </div>
                 </div>
-            </div>
+            </DndProvider>
         )
     }
 }
