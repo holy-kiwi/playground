adeda3c3cb4b68c467a9df6914a4eaadf59fcad9
jiwon_lala <s26788761@naver.com>
2019-09-04 11:57:04 +0900

    pluginItem에 Antd Card 적용

--- src/screens/PluginItem.tsx
+++ src/screens/PluginItem.tsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import "./PluginItem.css";
+import { Card, Icon, Avatar } from 'antd';
+import Meta from 'antd/lib/card/Meta';
 
 
 interface Props {
@@ -18,21 +20,44 @@ class PluginItem extends Component<{name: string; image: string; detail: string;
     render(){
         const {name, image, detail, download} = this.props;
         return (
-            <div className="plugin_box">
-                <h1 className="plugin_title" id="name">{name}</h1>
-                <img className="plugin_image" src={image} alt={name}/>
-                <div className="plugin_link">
+            // <div className="plugin_box">
+            //     <h1 className="plugin_title" id="name">{name}</h1>
+            //     <img className="plugin_image" src={image} alt={name}/>
+            //     <div className="plugin_link">     
+            //         <div className="plugin_link_detail">
+            //             <a href={detail}>Detail</a>
+            //         </div>
+            //         <div className="plugin_link_download">
+            //             <a href={download}>Download</a>
+            //         </div>        
+            //     </div>
+            // </div>
+
+            <Card
+                style={{ width: "30%"}}
+                cover={
                     
-                    <div className="plugin_link_detail">
-                        <a href={detail}>Detail</a>
+                    <div className="thumbnail-wrapper">
+                        <div className="thumbnail">
+                            <div className="centered">
+                                <img alt = {name} src = {image} />
+                            </div>
+                        </div>
                     </div>
-                
-                    <div className="plugin_link_download">
-                        <a href={download}>Download</a>
-                    </div>
-                    
-                </div>
-            </div>
+                }
+                actions= {[
+                    <Icon type="info-circle" key="detail" />,
+                    <Icon type= "cloud-download" key="download"/>
+                ]}
+            >
+                <Meta
+                    avatar = {<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                    title = {name}
+                    description = {name}
+                />
+
+            </Card>
+
         )
     }
 }
