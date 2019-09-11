f98899138802f02e4d4d1586d8ad5dc814234b16
jiwon_lala <s26788761@naver.com>
2019-09-11 13:12:07 +0900

    디테일 페이지 디자인 적용. 현재 브랜치를 서버연동 안되는 버젼으로 따서 후에 서버 연동 필요

--- src/screens/PluginDetailScreen.tsx
+++ src/screens/PluginDetailScreen.tsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'; 
 import { match } from 'react-router';
 import { Location } from 'history';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
+import './PluginDetailScreen.css';
+
 
 
 interface Props {
@@ -23,47 +27,63 @@ class PluginDetailScreen extends Component<Props, State> {
     }
 
     render() {
-        
-        // // const {Header, Content, Footer } = Layout;
-        // const  match = this.props.match;
-        // const match2 = {match}.match;
-
-        // const location = this.props.location;
-        // const location2 = {location}.location;
-
-        // console.log(match2);
-        // console.log(location2);
-        
-        //여기서 id값으로 서버에 질문해서 플러그인 정보 받아옴
         const plugin_name = "플러그인 이름";
         const plugin_subtitle = "플러그인 섭 타이틀(있으면)";
         const { match, location } = this.props;
 
         return (
-            <div>
-                {/* <Header>
-                    <PageHeader onBack={()=>null} title={plugin_name} subTitle={plugin_subtitle} />
-                </Header>
-                <Content>
-                    <img src="https://i.stack.imgur.com/GNhxO.png" />
-                    <p>this part is for descriptions.</p>
-                </Content>
-                <Footer></Footer> */}
-                
-                <h2><strong>{plugin_name}</strong> 디테일 페이지 입니다.</h2>
-                <h5>{plugin_subtitle}</h5>
-                <img src="https://i.stack.imgur.com/GNhxO.png" />
-                <p>
-                    설명설명
-                    <br/>
-                    location.pathname : {location.pathname}
-                    <br/>
-                    match.path : {match.path}
-                    <br/>
-                    match.url : {match.url}
-                    <br/>
-                    id: {match.params.id}
-                </p>
+            <div className="detailScreen">
+
+                {/* header */}
+                <div className="header">
+                  
+                  <div className="title_container">
+                    <h1>플러그인 스토어</h1>
+                  </div>
+        
+                  <div className="btn_container1">
+                    <Button type="primary" size="large" shape="round">정보</Button>
+                    <Button type="primary" size="large">유틸리티</Button>
+                    <Button type="primary" size="large">추천순</Button>
+                  </div>
+    
+                </div>
+                {/* end of header */}
+
+        
+                <div className="btn_container2">
+                  <Link to={"/"}>
+                    <button className="home_btn">홈</button>
+                  </Link>
+                </div>
+        
+        
+                {/* body */}
+                <div className="body detail">
+                    <div className="pluginTitleContainer">
+                        <h2><strong>{plugin_name}</strong> 디테일 페이지 입니다.</h2>
+                        <h5>{plugin_subtitle}</h5>
+                    </div>
+                    
+                    <div className="pluginImageContainer">
+                        <img src="https://i.stack.imgur.com/GNhxO.png" />
+                    </div>
+                    
+                    <div className="pluginDescriptionContainer">
+                        <p>
+                            설명설명
+                            <br/>
+                            location.pathname : {location.pathname}
+                            <br/>
+                            match.path : {match.path}
+                            <br/>
+                            match.url : {match.url}
+                            <br/>
+                            id: {match.params.id}
+                        </p>
+                    </div>
+
+                </div>
 
             </div>
         );
