7fc0bd49706245325d1ddb7ec91eeee83424649a
jiwon_lala <s26788761@naver.com>
2019-09-19 04:14:11 +0900

    디데이 플러그인 제작중_message localStoreage에 저장 기능 구현

--- example/dday/index.js
+++ example/dday/index.js
@@ -1,6 +1,6 @@
 const message_input = document.getElementById("message_input");
 
-const  down= document.getElementById("down");
+const down = document.getElementById("down");
 const date_msg = document.getElementById("date_msg");
 const date_picker = document.getElementById("date_picker");
 
@@ -9,48 +9,61 @@ const dday = document.getElementById("dday");
 const DEFAULT_MSG = "type the message..."
 const MSEC_PER_DAY = 24*60*60*1000;
 
-let date_picker_clicked = false;
-
 const KEY_DDAY_MSG = "key:dday_msg";
 const KEY_DDAY_DAY = "key:dday_day";
 
+function replaceall(str, searchStr, replaceStr) {
+    return str.split(searchStr).join(replaceStr);
+}
 
-// message_input.addEventListener('click', e => {
-//     e.target.placeholder = "";
-// });
+function loadMsg() {
+    message_input.value = localStorage.getItem(KEY_DDAY_MSG);
+    if(message_input.value===""){
+        message_input.value = DEFAULT_MSG;
+    }
+}
+
+//메시지 부분 누르면 초기화 되는 부분
 message_input.onclick = () => {
     message_input.value = "";
 }
-// message_input.addEventListener('change', e => {
-//     if (e.target.value===""){
-//         e.target.placeholder = DEFAULT_MSG;
-//     }
-// });
 
-// message_input.onkeydown = () => {
-//     if (message_input.value === ""){
-//         message_input.value= DEFAULT_MSG;
-//     }
-// }
+//메시지 localStorage에 저장하고 받아오는 부분
+loadMsg();
+message_input.onkeyup = () => {
+    // console.log(message_input.value);
+    localStorage.setItem(KEY_DDAY_MSG, message_input.value);
+};
+
+//메시지 빈칸상태인데 다른 곳으로 focus 이동할때 처리하는 부분
 message_input.onblur = () => {
     if (message_input.value === ""){
         message_input.value= DEFAULT_MSG;
     }
-
 };
 
-down.addEventListener('click', e => {
-    date_picker_clicked = !date_picker_clicked;
 
-    if (date_picker_clicked) {
-        date_msg.style.setProperty('display', 'none');
-        date_picker.style.setProperty('display', 'inline');
-    } 
-});
+//하부 클릭하면 date_picker 활성화 시켜주고 date_msg 비활성화 시키는 부분
+down.onclick = () => {
+    
+    date_msg.style.setProperty('display', 'none');
+    date_picker.style.setProperty('display', 'inline');
+    date_picker.focus();
+}
 
-date_picker.addEventListener('change', e => {
-    const picked_day = e.target.value;
-    const pday = new Date(picked_day).getTime();
+//하부 클릭했다가 다른데 클릭하면 date_picker 비활성화 시켜주고 date_msg 활성화 시키는 부분
+date_picker.onblur = () => {
+    
+    date_msg.style.setProperty('display', 'inline');
+    date_picker.style.setProperty('display', 'none');
+
+    if(date_picker.value != "") {
+        date_msg.value = replaceall(date_picker.value, "-", ".");
+    }
+}
+//date_picker에서 날짜를 고르면 현재 날짜와 비교하여 적절한 메시지("D-XXX" or "XXX일") 설정해주는 부분
+date_picker.addEventListener('change', (e)=> {
+    const pday = new Date(e.target.value).getTime();
     const nday = new Date().getTime();
     let diff = Math.floor((pday - nday)/MSEC_PER_DAY)+1;
 
@@ -62,7 +75,6 @@ date_picker.addEventListener('change', e => {
         dday.innerHTML = `D-${diff}`;
     }
 })
-
 
 
 
