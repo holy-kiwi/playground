d286b142beea7f86bec38590fe6fee8792e4c731
jiwon_lala <s26788761@naver.com>
2019-09-21 21:50:27 +0900

    DetailPage 디자인 적용 중

--- src/presentational/PluginDetail.tsx
+++ src/presentational/PluginDetail.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import "./PluginItem.css";
+// import "./PluginItem.css";
 import { Card, Skeleton, Button } from 'antd';
 import Plugin from '../models/Plugin';
+import './PluginDetail2.css';
 
 interface Props {
     plugin: Plugin;
@@ -24,26 +25,48 @@ const PluginDetail = (props: Props) => {
                 </Skeleton>
             </Card>
             :
-            <div className="body detail">
-                <div className="pluginTitleContainer">
-                    {/* 제목부분 간격 조정 필요 */}
-                    <h2><strong>{plugin.manifest.name}</strong></h2>
+            // <div className="body detail">
+            //     <div className="pluginTitleContainer">
+            //         {/* 제목부분 간격 조정 필요 */}
+            //         <h2><strong>{plugin.manifest.name}</strong></h2>
+            //     </div>
+
+            //     <div className="pluginImageContainer">
+            //         <img src={plugin.manifest.image} />
+            //     </div>
+
+            //     <div className="pluginDescriptionContainer">
+            //         <p>
+            //             {plugin.manifest.description}
+            //         </p>
+            //     </div>
+
+            //     <div className="btn_container4">
+            //         <Button className="download_btn" onClick={onDownload}>다운로드</Button>
+            //     </div>
+
+            // </div>
+
+            <div className="plugindetail"> 
+                <div className="plugin_info">
+                    <div className="image_container">
+                        {plugin.manifest.image &&<img src={plugin.manifest.image} alt={plugin.manifest.name}/>}
+                    </div>
+
+                    <div className="info_container">
+                        <div className="name"></div>
+                        <div className="uploader"></div>
+                        <div className="version"></div>
+                        <div className="updated_date"></div>
+                    </div>
+
+                    <div className="btn_container">
+                        <button className="download_btn" disabled={loading} onClick={onDownload}>DOWNLOAD</button>    
+                    </div>
                 </div>
 
-                <div className="pluginImageContainer">
-                    <img src={plugin.manifest.image} />
+                <div className="plugin_description">
                 </div>
-
-                <div className="pluginDescriptionContainer">
-                    <p>
-                        {plugin.manifest.description}
-                    </p>
-                </div>
-
-                <div className="btn_container4">
-                    <Button className="download_btn" onClick={onDownload}>다운로드</Button>
-                </div>
-
             </div>
     );
 }
--- src/screens/PluginDetailScreen.tsx
+++ src/screens/PluginDetailScreen.tsx
@@ -14,22 +14,11 @@ class PluginDetailScreen extends Component<Props> {
         return (
             <div className="detailScreen">
 
-                {/* header */}
-                <div className="header">
-
+                {/* <div className="header">
                     <div className="title_container">
                         <h1>플러그인 스토어</h1>
                     </div>
-
-                    {/* <div className="btn_container1">
-                        <Button type="primary" size="large" shape="round">정보</Button>
-                        <Button type="primary" size="large">유틸리티</Button>
-                        <Button type="primary" size="large">추천순</Button>
-                    </div> */}
-
                 </div>
-                {/* end of header */}
-
 
                 <div className="btn_container2">
                     <Link to={"/"}>
@@ -37,8 +26,27 @@ class PluginDetailScreen extends Component<Props> {
                     </Link>
                 </div>
 
+                <PluginDetailContainer /> */}
 
-                {/* body */}
+
+
+
+
+
+
+
+
+
+
+                <div className="header2">
+                    <Link to={"/"}>{'PLAYGROUND'}</Link>
+                    <div className="search_container" >
+                        <input className="search_bar" placeholder="Search Plugins!" />
+                        <div className="search_btn"> </div>
+                    </div>
+                </div>
+
+
                 <PluginDetailContainer />
 
             </div>
