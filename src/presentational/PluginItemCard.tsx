f1ea23370ab81d3f8da9613923f4cf3b1b0ff954
Jon Jee <jydrogen@gmail.com>
2019-09-21 20:21:22 +0900

    fetchplugins undefined 처리

--- src/presentational/PluginItemCard.tsx
+++ src/presentational/PluginItemCard.tsx
@@ -11,11 +11,25 @@ interface Props {
 }
 
 const PluginItemCard = (props: Props) => {
-    const { plugin, loading, onDownload } =  props;
-    
+    const { plugin, loading, onDownload } = props;
+
     return (
-        loading?
-            
+        !loading && plugin !== undefined && plugin.manifest !== undefined ?
+            <div className="PluginItemCard">
+                <div className="image_container">
+                    {plugin.manifest.image && <img src={plugin.manifest.image} alt={plugin.manifest.name} />}
+                </div>
+
+                <div className="info_container">
+                    <div className="title">{plugin.manifest.name}</div>
+                    <div className="description">{plugin.manifest.description}</div>
+                    <button className="download_btn" disabled={loading} onClick={onDownload}>DOWNLOAD</button>
+                    <Link to={`/store/${plugin.plugin_id}`}>
+                        <button className="detail_btn" disabled={loading}>SHOW DETAILS</button>
+                    </Link>
+                </div>
+            </div>
+            :
             <div className="PluginItemCard">
                 <div className="image_container">
                     <img src="https://bhmlib.org/wp-content/themes/cosimo-pro/images/no-image-box.png" />
@@ -24,29 +38,14 @@ const PluginItemCard = (props: Props) => {
                 <div className="info_container">
                     <div className="title">loading...</div>
                     <div className="description">loading...</div>
-                    <button className="download_btn" disabled={loading} onClick={onDownload}>DOWNLOAD</button>    
+                    <button className="download_btn" disabled={loading} onClick={onDownload}>DOWNLOAD</button>
                     <button className="detail_btn" disabled={loading}>SHOW DETAILS</button>
-                    
-                </div>
-            </div>
-            :
-            <div className="PluginItemCard">
-                <div className="image_container">
-                    {plugin.manifest.image &&<img src={plugin.manifest.image} alt={plugin.manifest.name}/>}
-                </div>
 
-                <div className="info_container">
-                    <div className="title">{plugin.manifest.name}</div>
-                    <div className="description">{plugin.manifest.description}</div>
-                    <button className="download_btn" disabled={loading} onClick={onDownload}>DOWNLOAD</button>    
-                    <Link to={`/store/${plugin.plugin_id}`}>
-                        <button className="detail_btn" disabled={loading}>SHOW DETAILS</button>
-                    </Link>
                 </div>
             </div>
 
     );
-    
+
 }
 
 export default PluginItemCard;
