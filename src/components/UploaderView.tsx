732b23c38df28862118d28be53c1c0a29ea44c82
jiwon_lala <s26788761@naver.com>
2019-09-22 20:56:12 +0900

    #34 PluginUploadScreen 디자인 적용 완료

--- src/components/UploaderView.tsx
+++ src/components/UploaderView.tsx
@@ -1,15 +1,41 @@
 import React, { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import image from '../assets/cloud-upload-alt-solid.png';
 
 const styles: React.CSSProperties = {
-    backgroundColor: '#ffffff50',
-    width: 200,
-    height: 100,
+    backgroundColor: '#f3f3f3',
+    width: '100%',
+    height: '202px',
     textAlign: 'center',
-    position: 'absolute',
-    left: 10,
-    top: 10,
+    // position: 'absolute',
+    // left: 10,
+    // top: 10,
     zIndex: 100,
+}
+
+const styles2: React.CSSProperties = {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: '50px',
+}
+
+const styles3: React.CSSProperties = {
+    width: '100%',
+    height: '302px',
+    fontFamily: 'NotoSansKR',
+    fontSize: '20px',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: '2',
+    letterSpacing: 'normal',
+    textAlign: 'center',
+    color: '#000000',
+
 }
 
 interface Props {
@@ -25,8 +51,11 @@ const UploaderView = (props: Props) => {
             <input {...getInputProps()} />
             {
                 isDragActive ?
-                    <div>놓아주세요</div> :
-                    <div>여기로 파일을 끌어주세요</div>
+                    <div style={styles3}>놓아주세요</div> :
+                    <div style={styles2}>
+                        <img src={image} alt="upload"/>
+                        <div style={styles3}>이곳으로 파일을 끌어주세요</div>
+                    </div>
             }
         </div>
     );
