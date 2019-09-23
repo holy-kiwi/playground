9c43c18ed4281cf1bb3157a42fb77ae8e75727e6
Jon Jee <jydrogen@gmail.com>
2019-09-23 14:08:20 +0900

    #25 네이버 검색 플러그인 스타일 개선

--- example/naver_search/index.js
+++ example/naver_search/index.js
@@ -1,6 +1,17 @@
-const search_naver_button = document.getElementById('search_naver_button');
+const KEY_NAVER = 'key:naversearch';
+const search_naver_button = document.getElementById('naversearch-button');
+const input = document.getElementById('naversearch-input');
 function search_naver() {
-	var keyword = document.getElementById('search_naver_keyword').value;
+	var keyword = input.value;
 	window.open(`https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=${keyword}`, '_blank');
 }
 search_naver_button.addEventListener('click', search_naver);
+
+input.value = localStorage.getItem(KEY_NAVER);
+
+input.addEventListener('keyup', e => {
+	localStorage.setItem(KEY_NAVER, e.target.value);
+	if (e.keyCode === 13) {
+		search_naver();
+	}
+});
