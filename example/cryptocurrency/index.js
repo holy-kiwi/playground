1a47b7c8ffb2623d23086d091e5d5833c6fb1971
Space0726 <woosuj07@naver.com>
2019-09-23 14:20:50 +0900

    Modify cryptocurrency plugin

--- example/cryptocurrency/index.js
+++ example/cryptocurrency/index.js
@@ -1,20 +1,31 @@
 var ex;
 var price;
-const crypto_name = document.getElementById('search_crypto_area')
 const crypto_quotes = document.getElementById('search_crypto_quotes');
-const crypto_button = document.getElementById('search_crypto_button');
 
-const search_exchange = () => {
+var crypto_list = [
+	'bitcoin',
+	'ethereum',
+	'ripple',
+	'bitcoin-cash',
+	'litecoin'
+];
+
+function make_id_name(name) {
+	return "id-" + name;
+}
+
+function number_with_commas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const search_exchange = (cb) => {
 	request(
 		'https://cors-anywhere.herokuapp.com/https://earthquake.kr:23490/query/USDKRW',
 		function(error, response, html) {
 			if (error) console.error(error);
 			if (!error && response.statusCode === 200) {
-				//var $ = cheerio.load(html);
 				ex = JSON.parse(cheerio.load(html).text())['USDKRW'][0];
-				//const result = $(
-				//	'div.PM_CL_realtimeKeyword_rolling'
-				//);
+				cb && cb();
 			}
 		}
 	)
@@ -26,13 +37,18 @@ const search_crypto_quotes = () => {
 		function(error, response, html) {
 			if (error) console.error(error);
 			if (!error && response.statusCode === 200) {
-				var name = crypto_name.value
-				price = cheerio.load(html)('tr#'.concat(name).concat(' .price')).text();
-				crypto_quotes.innerHTML = Math.round(Number(ex)*Number(price.substr(1)));
+				var quotes_str = '<h3><b>- 주요 암호화폐 시세 -</b></h3>';
+				for (var name of crypto_list) {
+					name = make_id_name(name);
+					price = cheerio.load(html)('tr#'.concat(name).concat(' .price')).text();
+					quotes_str += '<li>' + name.substring(3) + ': '
+								  + number_with_commas(Math.round(Number(ex)*Number(price.substr(1))))
+								  + '원</li>';
+				}
+				crypto_quotes.innerHTML = quotes_str;
 			}
 		}
 	)
 };
 
-search_exchange();
-crypto_button.addEventListener('click', search_crypto_quotes);
+search_exchange(search_crypto_quotes);
