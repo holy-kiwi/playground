57240ad9eec0482b5bf6cc2d286219b63ce089d1
Space0726 <woosuj07@naver.com>
2019-09-23 13:09:52 +0900

    Remove test files

--- example/naver_realtime/test/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('request');
-var cheerio = require('cheerio');
-
-res = request.get({url:"https://www.naver.com/"}, function(err, res, body) {
-	var $ = cheerio.load(body);
-	const result = $('div.PM_CL_realtimeKeyword_rolling');
-	console.log(result.text());
-});
